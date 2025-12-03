9aff6d4b83328477d415aa9fe8a6accc3d0d44a9
liquid-veg <liquid@veg.com>
2025-12-03 22:49:26 +0000

    Part 2 complete.

--- day3/main.cpp
+++ day3/main.cpp
@@ -11,13 +11,12 @@ int main()
     std::vector<std::string> joltages;
     std::string line;
     std::ifstream fData("data/data");
-    int total = 0;
+    long total = 0;
 
     if (fData.is_open()){
         while(getline(fData,line)){
             batteries.push_back(line);
-            //std::cout << batteries.back() << std::endl;
-            joltages.push_back(std::string());
+            joltages.push_back(std::string(""));
         }
         fData.close();
     }
@@ -25,30 +24,48 @@ int main()
     int cntr = 0;
     for(std::string bank : batteries){
 
-        int max1 = 0;
         int maxPos1 = 0;
+        int max1 = 0;
+        std::vector<int> maxPos(12);
 
-        for (int i=bank.length()-2;i>=0;i--){
+        for (int i=bank.length()-12;i>=0;i--){
             int num = stoi(bank.substr(i,1));
             if (num >= max1){
                 max1 = num;
-                maxPos1 = i;
+                maxPos1=i;
             }
         }
-        
-        int max2 = 0;
-        int maxPos2 = 0;
-        for (int i=maxPos1+1;i<bank.length();i++){
-            int num = stoi(bank.substr(i,1));
-            if (num >= max2){
-                max2 = num;
-                maxPos2 = i;
-            }
+        maxPos[0] = maxPos1;
+        std::cout << bank.substr(maxPos[0],1);
+
+        if (maxPos[0] == bank.length()-12){
+            joltages[cntr] = bank.substr(bank.length()-12);
         }
-        std::cout << bank << "   max: " << bank.substr(maxPos1,1) << bank.substr(maxPos2,1) << std::endl;
-        joltages[cntr] = bank.substr(maxPos1,1) + bank.substr(maxPos2,1);
-        total += stoi(joltages[cntr]);
+        else{        
+            for (int k=1;k<12;k++){
+                int maxPos2 = 0;
+                int max2 = 0;
+                for (int i=maxPos[k-1]+1;i<bank.length()-(11-k);i++){
+                    int num = stoi(bank.substr(i,1));
+                    if (num > max2){
+                        max2 = num;
+                        maxPos2 = i;
+                    }
+                }
+                maxPos[k] = maxPos2;
+                std::cout << bank.substr(maxPos[k],1);
+            }
+            std::cout << std::endl;
+            
+            for (int i=0;i<12;i++){
+                joltages[cntr] = joltages[cntr] + bank.substr(maxPos[i],1);
+            }
+            std::cout << bank << "   max: " << joltages[cntr]<< std::endl;
+            
+        }
         
+        total += stol(joltages[cntr]);
+        cntr++;
     }
     std::cout << "Total: " << total << std::endl;
 
